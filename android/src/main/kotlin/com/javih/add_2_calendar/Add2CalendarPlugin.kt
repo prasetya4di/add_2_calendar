28ff33598a5d75674704a303e831dd0c1285ec7e
prasetya <prasetyaarfanto@gmail.com>
2024-11-29 11:37:45 +0700

    Update android plugin code

--- android/src/main/kotlin/com/javih/add_2_calendar/Add2CalendarPlugin.kt
+++ android/src/main/kotlin/com/javih/add_2_calendar/Add2CalendarPlugin.kt
@@ -19,46 +19,41 @@ import java.util.*
 
 
 /** Add2CalendarPlugin */
-class Add2CalendarPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
-  private var activity: Activity? = null
-  private var context: Context? = null
+class Add2CalendarPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+    private lateinit var channel: MethodChannel
+    private var activity: Activity? = null
+    private var context: Context? = null
 
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    context = flutterPluginBinding.applicationContext
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "add_2_calendar")
-    channel.setMethodCallHandler(this)
-  }
- 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-      if (call.method == "add2Cal") {
-          val success = insert(
-              call.argument("title")!!,
-              call.argument("desc") as String?,
-              call.argument("location") as String?,
-              call.argument("startDate")!!,
-              call.argument("endDate")!!,
-              call.argument("timeZone") as String?,
-              call.argument("allDay")!!,
-              call.argument("recurrence") as HashMap<String, Any>?,
-              call.argument("invites") as String?
-          )
-          result.success(success)
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        context = flutterPluginBinding.applicationContext
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "add_2_calendar")
+        channel.setMethodCallHandler(this)
+    }
 
-      } else {
-          result.notImplemented()
-      }
-  }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method == "add2Cal") {
+            val success = insert(
+                call.argument("title")!!,
+                call.argument("desc") as String?,
+                call.argument("location") as String?,
+                call.argument("startDate")!!,
+                call.argument("endDate")!!,
+                call.argument("timeZone") as String?,
+                call.argument("allDay")!!,
+                call.argument("recurrence") as HashMap<String, Any>?,
+                call.argument("invites") as String?
+            )
+            result.success(success)
+        } else {
+            result.notImplemented()
+        }
+    }
 
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 
- override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
         activity = binding.activity
     }
 
@@ -77,7 +72,7 @@ class Add2CalendarPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
     private fun insert(
         title: String,
         desc: String?,
-        loc: String?,
+        location: String?,
         start: Long,
         end: Long,
         timeZone: String?,
@@ -85,76 +80,63 @@ class Add2CalendarPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
         recurrence: HashMap<String, Any>?,
         invites: String?
     ): Boolean {
-
-        val mContext: Context = if (activity != null) activity!!.applicationContext else context!!
-        val intent = Intent(Intent.ACTION_INSERT)
-
-
-        intent.data = CalendarContract.Events.CONTENT_URI
-        intent.putExtra(CalendarContract.Events.TITLE, title)
-
-        if (desc != null) {
-            intent.putExtra(CalendarContract.Events.DESCRIPTION, desc)
+        val mContext: Context = activity?.applicationContext ?: context!!
+        val intent = Intent(Intent.ACTION_INSERT).apply {
+            data = CalendarContract.Events.CONTENT_URI
+            putExtra(CalendarContract.Events.TITLE, title)
+            putExtra(CalendarContract.Events.DESCRIPTION, desc)
+            putExtra(CalendarContract.Events.EVENT_LOCATION, location)
+            putExtra(CalendarContract.Events.EVENT_TIMEZONE, timeZone)
+            putExtra(CalendarContract.Events.EVENT_END_TIMEZONE, timeZone)
+            putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, start)
+            putExtra(CalendarContract.EXTRA_EVENT_END_TIME, end)
+            putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, allDay)
+            putExtra(CalendarContract.Events.RRULE, buildRRule(recurrence))
+            putExtra(Intent.EXTRA_EMAIL, invites)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         }
 
-        if (loc != null) {
-            intent.putExtra(CalendarContract.Events.EVENT_LOCATION, loc)
-        }
-
-        intent.putExtra(CalendarContract.Events.EVENT_TIMEZONE, timeZone)
-        intent.putExtra(CalendarContract.Events.EVENT_END_TIMEZONE, timeZone)
-        intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, start)
-        intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, end)
-        intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, allDay)
-
-        if (recurrence != null) {
-            intent.putExtra(CalendarContract.Events.RRULE, buildRRule(recurrence))
-        }
-
-        if (invites != null) {
-            intent.putExtra(Intent.EXTRA_EMAIL, invites)
-        }
-
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-
-        if (intent.resolveActivity(mContext.packageManager) != null) {
+        return if (intent.resolveActivity(mContext.packageManager) != null) {
             mContext.startActivity(intent)
-            return true
+            true
+        } else {
+            false
         }
-
-        return false
     }
 
-
-    private fun buildRRule(recurrence: HashMap<String, Any>): String? {
+    private fun buildRRule(recurrence: HashMap<String, Any>?): String? {
+        if (recurrence == null) return null
 
         var rRule = recurrence["rRule"] as String?
         if (rRule == null) {
-            rRule = ""
-            val freqEnum: Int? = recurrence["frequency"] as Int?
-            if (freqEnum != null) {
-                rRule += "FREQ="
-                when (freqEnum) {
-                    0 -> rRule += "DAILY"
-                    1 -> rRule += "WEEKLY"
-                    2 -> rRule += "MONTHLY"
-                    3 -> rRule += "YEARLY"
+            rRule = buildString {
+                val freqEnum = recurrence["frequency"] as Int?
+                if (freqEnum != null) {
+                    append("FREQ=")
+                    append(
+                        when (freqEnum) {
+                            0 -> "DAILY"
+                            1 -> "WEEKLY"
+                            2 -> "MONTHLY"
+                            3 -> "YEARLY"
+                            else -> ""
+                        }
+                    )
+                    append(";")
                 }
-                rRule += ";"
-            }
-            rRule += "INTERVAL=" + recurrence["interval"] as Int + ";"
-            val occurrences: Int? = recurrence["ocurrences"] as Int?
-            if (occurrences != null) {
-                rRule += "COUNT=" + occurrences.toInt().toString() + ";"
-            }
-            val endDateMillis = recurrence["endDate"] as Long?
-            if (endDateMillis != null) {
-                val endDate = Date(endDateMillis)
-                val formatter: DateFormat = SimpleDateFormat("yyyyMMdd'T'HHmmss")
-                rRule += "UNTIL=" + formatter.format(endDate).toString() + ";"
+                append("INTERVAL=")
+                append(recurrence["interval"] as Int)
+                append(";")
+                (recurrence["ocurrences"] as Int?)?.let {
+                    append("COUNT=$it;")
+                }
+                (recurrence["endDate"] as Long?)?.let { endDateMillis ->
+                    val endDate = Date(endDateMillis)
+                    val formatter = SimpleDateFormat("yyyyMMdd'T'HHmmss", Locale.getDefault())
+                    append("UNTIL=${formatter.format(endDate)};")
+                }
             }
         }
         return rRule
     }
-
 }
